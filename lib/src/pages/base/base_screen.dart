9b9d55bcbb4c7fdca8e7df69410ba938f55768a4
QuocDat <74419001+QuocDatA@users.noreply.github.com>
2023-04-12 16:04:31 +0700

    Login-SignUp-Home-Detail-Profile-Dialog

---
+++ lib/src/pages/base/base_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:food_app_getx/src/pages/cart/cart_tab.dart';
+import 'package:food_app_getx/src/pages/profile/profile_tab.dart';
+
+import '../home/home_tab.dart';
+
+class BaseScreen extends StatefulWidget {
+  const BaseScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+
+  int currentIndex = 0;
+  final pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          const HomeTab(),
+          const CartTab(),
+          Container(color: Colors.blue),
+          const ProfileTab()
+        ]
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+            pageController.jumpToPage(index);
+          });
+        } ,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.green,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withAlpha(100),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: "Cart",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: "Order",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: "Profile",
+          ),
+        ],
+      ),
+    );
+  }
+}
